3eb7099ecbc2d69e8b52460d41c9ffe3ca79bc96
ARANCIO <gonzalesinciofabioleonel@gmail.com>
2023-10-23 01:18:45 -0500

    si

--- app/api/available-dates/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function GET(req:any) {
-    const response = await fetch("http://localhost:5000/available-dates", {headers: {
-        "Content-Type": "application/json"
-    }, cache: "no-cache"})
-    const data = await response.json()
-    return NextResponse.json(data)
-}
