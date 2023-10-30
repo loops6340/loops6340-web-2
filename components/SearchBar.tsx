6773646e4610c881fe03fa65edc5d1f4176c76b8
ARANCIO <gonzalesinciofabioleonel@gmail.com>
2023-10-30 00:16:46 -0500

    ahora se puede buscar en yt

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,7 +6,6 @@ import Calendar from "react-calendar"
 import { useRouter } from "next/navigation";
 type ValuePiece = Date | null;
 import Modal from 'react-modal';
-
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 
@@ -28,14 +27,34 @@ Modal.setAppElement('body');
 const SearchBar = () => {
  
 
+
+
+    const [busqueda, setBusqueda] = useState("")
+    const [id, setId] =  useState("")
+    
+
+    const router = useRouter()
     return (
         <>
-            <form action={""} className="flex">
-                <input type="text" className="h-[100%] w-[100%] lg:w-[584px]" readOnly value={""} />
-                <Button onClick={(e) => {
-                
-                }}
-                    disabled={false}
+            <form onSubmit={async (e) => {
+                e.preventDefault()
+                console.log("HOLAAA HUMANOS XD")
+               
+                // const response = await fetch(`/api/youtube/${busqueda}`)
+                // const json = await response.json()
+                router.push(`/youtube/${encodeURIComponent(busqueda)}`)
+            }} className="flex">
+                <input 
+                    type="text" 
+                    className="h-[100%] w-[100%] lg:w-[584px]" 
+                    // placeholder="busca" 
+                    value={busqueda} 
+                    onChange={(e) => {
+                        setBusqueda(e.currentTarget.value)
+                    }}
+                />
+                <Button
+                    disabled={!Boolean(busqueda)}
                 >
                     <span className="bg-no-repeat bg-[url(https://web.archive.org/web/20150611000903im_/https://s.ytimg.com/yts/imgbin/www-hitchhiker-vfldzd94_.png)] bg-search w-[14px] h-[14px] block text-center mr-[0.95rem] ml-[0.95rem] opacity-[.6]"></span>
                 </Button>
