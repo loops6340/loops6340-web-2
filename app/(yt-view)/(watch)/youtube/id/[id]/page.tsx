dbe0125626a6403a1deb3e0041c40ad37d28d779
Loops Aso <gonzalesinciofabioleonel@gmail.com>
2023-11-04 01:08:54 -0500

    prueba 12

--- app/(yt-view)/(watch)/youtube/id/[id]/page.tsx
+++ app/(yt-view)/(watch)/youtube/id/[id]/page.tsx
@@ -7,83 +7,84 @@ import SeccionRecomendados from "./recomendados";
 import { Suspense } from "react";
 import { Metadata } from "next";
 import dynamic from "next/dynamic";
+import Recomendados from "./recomendados";
+import Contenido from "./contenido";
+// async function search(search: string) {
+//     console.log("TARDO", search)
+//     console.time()
+//     const searchResult = await ytsr.getFilters(search)
+//     console.log(searchResult)
+//     console.log("YANO")
+//     const f = searchResult.get("Type")?.get("Video")
+//     const a = await ytsr(f!.url!, { pages: 1 })
+//     console.log(f!.url)
+//     console.timeEnd()
+//     return a.items as ytsr.Video[]
+// }
+// type Props = {
+//     params: { id: string, search: string }
+//     searchParams: { [key: string]: string | undefined }
+//   }
+//   const DynamicRecomendados = dynamic(() => import('./recomendados'), {
+//     loading: () => <div>LOADING</div>
+//   })
 
-async function search(search: string) {
-    console.log("TARDO", search)
-    console.time()
-    const searchResult = await ytsr.getFilters(search)
-    console.log(searchResult)
-    console.log("YANO")
-    const f = searchResult.get("Type")?.get("Video")
-    const a = await ytsr(f!.url!, { pages: 1 })
-    console.log(f!.url)
-    console.timeEnd()
-    return a.items as ytsr.Video[]
-}
-type Props = {
-    params: { id: string, search: string }
-    searchParams: { [key: string]: string | undefined }
-  }
-  const DynamicRecomendados = dynamic(() => import('./recomendados'), {
-    loading: () => <div>LOADING</div>
-  })
-
-  // export const metadata = {
-    const DynamicContenido = dynamic(() => import('./contenido'), {
+//   // export const metadata = {
+//     const DynamicContenido = dynamic(() => import('./contenido'), {
     
-    loading: () => <div>LOADING</div>
-    })
+//     loading: () => <div>LOADING</div>
+//     })
 //     other: {
 //         "theme-color": "#43B581"
 //     }
 // }
-export async function generateMetadata({searchParams, params}: Props): Promise<Metadata> {
+// export async function generateMetadata({searchParams, params}: Props): Promise<Metadata> {
 
-    // const idSearchResult = await search(`https://youtube.com/watch?v=${decodeURIComponent(searchParams!.v!)}`)
+//     // const idSearchResult = await search(`https://youtube.com/watch?v=${decodeURIComponent(searchParams!.v!)}`)
 
 
     
-    let videos: ytsr.Video[] = [];
+//     let videos: ytsr.Video[] = [];
 
-    if (params.id) {
+//     if (params.id) {
      
-            const querySearchResult = await search(decodeURIComponent(params.id))
-            // console.log(querySearchResult)
-            videos = querySearchResult
+//             const querySearchResult = await search(decodeURIComponent(params.id))
+//             // console.log(querySearchResult)
+//             videos = querySearchResult
         
-    } else {
-        // const idSearchResult = await search(`https://youtube.com/watch?v=${decodeURIComponent(searchParams.v)}`)
-        // // console.log(idSearchResult)
-        // videos = idSearchResult
-        // recomendados =  await search(decodeURIComponent(video.title))
-    }
+//     } else {
+//         // const idSearchResult = await search(`https://youtube.com/watch?v=${decodeURIComponent(searchParams.v)}`)
+//         // // console.log(idSearchResult)
+//         // videos = idSearchResult
+//         // recomendados =  await search(decodeURIComponent(video.title))
+//     }
 
-    return {
-            other: {
-        "theme-color": "#43B581"
-    },
-        title: videos[0].title,
-        alternates: {
-            types: {
-                "application/json+oembed": `https://www.youtube.com/oembed?format=json&amp;url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D${videos[0].id}`
-            }
-        },
-        openGraph: {
-            url: `https://youtube.com/watch?v=${videos[0].id}`,
-            // title: videos[0].title,
-            images: `https://i.ytimg.com/vi/${videos[0].id}/maxresdefault.jpg`,
-            videos: {
-                type: "text/html",
-                url: `https://youtube.com/embed/${videos[0].id}`,
-                height: 720,
-                width: 1280,
+//     return {
+//             other: {
+//         "theme-color": "#43B581"
+//     },
+//         title: videos[0].title,
+//         alternates: {
+//             types: {
+//                 "application/json+oembed": `https://www.youtube.com/oembed?format=json&amp;url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D${videos[0].id}`
+//             }
+//         },
+//         openGraph: {
+//             url: `https://youtube.com/watch?v=${videos[0].id}`,
+//             // title: videos[0].title,
+//             images: `https://i.ytimg.com/vi/${videos[0].id}/maxresdefault.jpg`,
+//             videos: {
+//                 type: "text/html",
+//                 url: `https://youtube.com/embed/${videos[0].id}`,
+//                 height: 720,
+//                 width: 1280,
 
-            },
-            type: "video.other",
+//             },
+//             type: "video.other",
             
-        },
-    }
-}
+//         },
+//     }
+// }
 export default async function Date(props: any) {
 
     return (
@@ -104,14 +105,19 @@ export default async function Date(props: any) {
                     ></iframe>
                 
             </div>
-            {/* <Suspense> */}
-            <DynamicContenido props={props}/>
+            <Suspense fallback={<div>LOADING</div>
+}>
+                <Contenido props={props}/>
 
-            {/* </Suspense> */}
+            </Suspense>
 
         </div>
            <div className="flex-1 flex flex-col bg-white">
-           <DynamicRecomendados {...props} />
+            <Suspense fallback={<div>LOADING</div>
+}>
+                <Recomendados {...props} />
+
+            </Suspense>
 
            </div>
 
